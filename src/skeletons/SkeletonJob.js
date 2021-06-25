38a2ffecce72c6b1240c28e6c83a3adf2dc1e4e4
Joseph Lynn <josephlynn94@gmail.com>
2021-06-25 09:56:36 -0400

    added another div to skeleton loader so image can overflow

--- src/skeletons/SkeletonJob.js
+++ src/skeletons/SkeletonJob.js
@@ -4,21 +4,23 @@ import SkeletonElement from './SkeletonElement';
 
 const SkeletonJob = () => {
   return (
-    <div className='skeleton-wrapper'>
-      <div className='skeleton-job'>
-        <div className='skeleton-job__top'>
-          <SkeletonElement type='thumbnail' />
-        </div>
-        <div className='skeleton-job__body'>
-          <SkeletonElement type='text' />
-          <SkeletonElement type='title' />
-          <SkeletonElement type='text' />
-        </div>
-        <div className='skeleton-job__footer'>
-          <SkeletonElement type='title' />
+    <div className='wrap'>
+      <div className='skeleton-wrapper'>
+        <div className='skeleton-job'>
+          <div className='skeleton-job__top'>
+            <SkeletonElement type='thumbnail' />
+          </div>
+          <div className='skeleton-job__body'>
+            <SkeletonElement type='text' />
+            <SkeletonElement type='title' />
+            <SkeletonElement type='text' />
+          </div>
+          <div className='skeleton-job__footer'>
+            <SkeletonElement type='title' />
+          </div>
+          <Shimmer />
         </div>
       </div>
-      <Shimmer />
     </div>
   );
 };
