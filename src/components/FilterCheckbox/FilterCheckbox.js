4b2dc86cb60df7b8a202ed20118d17a09852f48c
Joseph Lynn <josephlynn94@gmail.com>
2021-07-02 14:02:22 -0400

    fixed DOM issues

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './FilterCheckbox.css';
 
-const FilterCheckbox = ({ label, onCheckboxChange, isChecked }) => {
+const FilterCheckbox = ({ label, onCheckboxChange }) => {
   return (
     <label className='ui-checkbox'>
-      <input type='checkbox' onClick={onCheckboxChange} isChecked={isChecked} />
+      <input type='checkbox' onClick={onCheckboxChange} />
       <span className='ui-checkbox--checkmark'></span>
       {label ? <span className='ui-checkbox__text'>{label}</span> : ''}
     </label>
